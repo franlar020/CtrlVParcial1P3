f7e82ca2eab09aaff8fe8146053182b1a2995850
Francisco Antonio Gonzalez <franlar020@gmail.com>
2026-05-18 09:50:28 -0300

    Agregar archivos de configuración y estructura inicial del proyecto Spring Boot

---
+++ src/main/java/unlar/edu/ar/CtrlVParcial1P3/DemoApplication.java
@@ -0,0 +1,13 @@
+package unlar.edu.ar.CtrlVParcial1P3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unlar/edu/ar/CtrlVParcial1P3/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package unlar.edu.ar.CtrlVParcial1P3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
