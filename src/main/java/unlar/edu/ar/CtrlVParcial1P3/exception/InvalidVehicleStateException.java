6e0dc07842e907221a6b2aa3a0531ff37552d341
IsmaDeveloper16 <ismatrumpet1616@gmail.com>
2026-06-18 21:12:08 -0300

    Agregar clase InvalidVehicleStateException: manejar excepciones de estado inválido del vehículo

---
+++ src/main/java/unlar/edu/ar/CtrlVParcial1P3/exception/InvalidVehicleStateException.java
@@ -0,0 +1,7 @@
+package unlar.edu.ar.CtrlVParcial1P3.exception;
+
+public class InvalidVehicleStateException extends RuntimeException {
+    public InvalidVehicleStateException(String message) {
+        super(message);
+    }
+}
