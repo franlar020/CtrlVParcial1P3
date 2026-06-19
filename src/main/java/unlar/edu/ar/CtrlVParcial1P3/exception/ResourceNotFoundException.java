9ef9f58f993bd23c06d83336aa5542315044e46b
IsmaDeveloper16 <ismatrumpet1616@gmail.com>
2026-06-18 21:09:55 -0300

    Agregar clase ResourceNotFoundException: manejar excepciones de recurso no encontrado

---
+++ src/main/java/unlar/edu/ar/CtrlVParcial1P3/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package unlar.edu.ar.CtrlVParcial1P3.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
